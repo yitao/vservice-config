28c14b51e8d9d62f8484bc6fafd0148e5ed8f6d6
yitao <yitao@vibeac.com>
2019-01-24 11:21:18 +0800

    初始化配置

---
+++ src/main/java/com/vservice/config/Application.java
@@ -0,0 +1,17 @@
+package com.vservice.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用启动类
+ * Created by yitao on 2019/1/24.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
